7247fc7313aa2c215633a9e67a3d1cd365b93a7e
RafiaSuleman <rafiasuleman47@gmail.com>
2024-10-09 20:07:41 +0500

    dynamic data fetch

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,10 @@
 export const round2 = (num: number) => Math.round((num + Number.EPSILON) * 100) /100
+
+export function convertDocToObj(doc: any) {
+    if (doc._id) {
+      doc._id = doc._id.toString();
+    } else {
+      console.warn('doc._id is undefined');
+    }
+    return doc;
+  }
--- src/app/(front)/product/[slug]/page.tsx
+++ src/app/(front)/product/[slug]/page.tsx
@@ -1,14 +1,32 @@
 import Link from "next/link";
-import data from "../../../../../lib/data";
 import Image from "next/image";
 import AddToCart from "../../../../../components/products/addtocart";
+import productService from "../../../../../lib/services/productservice";
+import { convertDocToObj } from "../../../../../lib/utils";
 
-export default function ProductDetails({
+// product name in the tab
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await productService.getBySlug(params.slug)
+  if (!product) {
+    return { title: 'Product not found' }
+  }
+  return {
+    title: product.name,
+    description: product.description,
+  }
+}
+
+
+export default async function ProductDetails({
   params,
 }: {
   params: { slug: string };
 }) {
-  const product = data.products.find((x) => x.slug === params.slug);
+  const product = await productService.getBySlug(params.slug)
   if (!product) {
     return <div>product not found</div>;
   }
@@ -58,7 +76,7 @@ export default function ProductDetails({
         </div>
         {product.countInStock !== 0 && (
           <div className="card-actions justify-center">
-            <AddToCart item={{...product , qty:0 , color: '' ,size: ''}  }/>
+            <AddToCart item={{...convertDocToObj( product) , qty:0 , color: '' ,size: ''}  }/>
           </div>)}
           
       </div>
